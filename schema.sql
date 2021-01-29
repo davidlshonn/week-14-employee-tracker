c2160553b0fa4257ca5087bbd4d4f606af652070
David Shonn <davidlshonn@icloud.com>
2021-01-29 18:35:04 +0000

    schema file completed

--- schema.sql
+++ schema.sql
@@ -9,26 +9,29 @@ USE employee_tracker_db;
 
 -- Created the table "department"
 CREATE TABLE department (
-  id int NOT NULL,
+  id int AUTO_INCREMENT NOT NULL,
   name varchar(30) NOT NULL,
   PRIMARY KEY(id)
 );
 
 -- Created the table "role"
 CREATE TABLE role (
-  id int NOT NULL,
+  id int AUTO_INCREMENT NOT NULL,
   title varchar(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
   PRIMARY KEY(id)
 );
 
 -- Created the table "employee"
 CREATE TABLE employee (
-  id int NOT NULL,
+  id int AUTO_INCREMENT NOT NULL,
   first_name varchar(30) NOT NULL,
   last_name varchar(30) NOT NULL,
   role_id int NOT NULL,
-  manager_id int --null?
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id int,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
   PRIMARY KEY(id)
 );
