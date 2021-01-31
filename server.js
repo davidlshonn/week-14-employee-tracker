a446340d5097588cf1164ef39b53d400dd077fba
David Shonn <davidlshonn@icloud.com>
2021-01-31 16:34:41 +0000

    view by role added + working

--- server.js
+++ server.js
@@ -59,6 +59,15 @@ function viewEmployees() {
 }
 
 function viewEmployeesByDepartment() {
+  connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;",
+  function (err, res) {
+    if (err) throw err
+    console.table(res)
+    userSelection()
+  })
+}
+
+function viewEmployeesByRole() {
   connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;",
   function (err, res) {
     if (err) throw err
