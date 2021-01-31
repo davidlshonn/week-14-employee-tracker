f5ca1adf2656f83d5004f627c5323cc75907adfb
David Shonn <davidlshonn@icloud.com>
2021-01-31 16:02:20 +0000

    view all employees prompt added and working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 var mysql = require("mysql");
+var inquirer = require('inquirer');
 
 // MySQL DB Connection Information
 var connection = mysql.createConnection({
@@ -11,10 +12,48 @@ var connection = mysql.createConnection({
 });
 
 // Initiate MySQL Connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+  if (err) throw err;
+  userSelection();
 });
+
+function userSelection() {
+  inquirer
+    .prompt({
+      name: 'action',
+      type: 'rawlist',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Employees',
+        'View all Employees By Role',
+        'View all Employees By Department',
+        'Add Employee',
+        'Remove Employee',
+        'Update Employee Role',
+        'Update Employee Manager'
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.action){
+        case 'View All Employees':
+          viewEmployees();
+          break;
+
+        case 'View all Employees By Role':
+          viewEmployeesByRole();
+          break;
+
+        case 'View all Employees By Department':
+          viewEmployeesByDepartment();         
+      }
+    });
+}
+
+function viewEmployees() {
+  connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+  function (err, res) {
+      if (err) throw err
+      console.table(res)
+      userSelection()
+  })
+}
